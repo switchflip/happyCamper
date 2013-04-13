87c6a06f2e4343e64b54cbf580e2672af6b622b7
Thomas Berry <thom.berry@gmail.com>
2013-04-12 20:52:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyCamper::Application.config.secret_token = '1843a497747ba4e6ca79cbc665b40e63c41fd59fe4d2b6779e658360100faa1502dd26f498d1298b8eeeec9380262eb33bf876d7c510d2447da7408cf68f67ef'
