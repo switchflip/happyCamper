e2ea3ab15fc7d8e4d116133b67123c5879ec3b1a
Emerson Lackey <emerson.lackey@gmail.com>
2013-04-13 22:03:42 -0400

    fixing create user form

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ HappyCamper::Application.routes.draw do
   get "home/index"
 
   get "logout" => "sessions#destory", :as => "logout"
-  get "login"  => "sessions#new", :as => "login"
-  get "signup" => "users#new", :as => "signup"
+  get "login"  => "sessions#new",     :as => "login"
+  get "signup" => "users#new",        :as => "signup"
 
   resources :users
   resources :sessions
