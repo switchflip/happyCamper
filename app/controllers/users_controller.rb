ada148805fd9bad1e65e80ba5a5ff6db4053ba35
Thomas Berry <thom.berry@gmail.com>
2013-04-13 21:54:54 -0400

    added a home controller and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice =>"Signed up!"
+      auto_login(@user)
+      redirect_to root_path, :notice =>"Account created!"
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HappyCamper::Application.routes.draw do
+  get "home/index"
+
   get "logout" => "sessions#destory", :as => "logout"
   get "login"  => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
